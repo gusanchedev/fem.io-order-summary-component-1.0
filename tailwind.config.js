2de6838fb520c1fd20c9b06d0e8bfa4431430def
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-08-24 20:12:39 -0500

    Add styling for mobile 375px width

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "red-hat":['"Red Hat Display"', 'sans-serif'],
+      },
+      colors: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)"
+      },
+      boxShadow: {
+        "card-button": "0 10px 8px 1px #dfdaff"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
